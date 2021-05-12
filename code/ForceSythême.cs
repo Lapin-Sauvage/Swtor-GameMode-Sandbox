5c071109fa15b3e6fb39dadad91a209e1d25041b
Lapin Pépère <83842166+Lapin-Sauvage@users.noreply.github.com>
2021-05-12 21:25:33 +0200

    System de force
    Ceci est le system de force !

---
+++ code/ForceSythême.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+namespace Mon_App_Dev
+{
+    class ForceSythême
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public void SysthemeForce()
+        {
+            // Event Moove qui va add des point d'age
+            string gradeforce;
+            var force_point = 0.000;
+
+            if (force_point == 100.000)
+            {
+                Console.WriteLine("Vous avez atteint les 100 points de force ! Vous êtes de plus en plus puissant ...");
+                gradeforce = "Adepte de la force ";
+            }
+
+            if (force_point == 1000.000)
+            {
+                Console.WriteLine("Vous avez atteint les 1000 points de force ! Vous êtes de plus en plus puissant ...");
+                gradeforce = "Savant de la force";
+            }
+
+            if (force_point == 5000.000)
+            {
+                Console.WriteLine("Vous avez atteint les 1000 points de force ! Vous êtes de plus en plus puissant ...");
+                gradeforce = "Maitriseur de la force";
+            }
+
+            if (force_point == 10000.000)
+            {
+                Console.WriteLine("Vous avez atteint les 1000 points de force ! Vous êtes de plus en plus puissant ...");
+                gradeforce = "Grand Adepte de la force";
+            }
+
+            if (force_point == 20000.000)
+            {
+                Console.WriteLine("Vous avez atteint les 1000 points de force ! Vous êtes de plus en plus puissant ...");
+                gradeforce = "Grand Savant de la force";
+            }
+
+            if (force_point == 30000.000)
+            {
+                Console.WriteLine("Vous avez atteint les 1000 points de force ! Vous êtes de plus en plus puissant ...");
+                gradeforce = "Grand Maitriseur de la force";
+            }
+
+        }
+    }
+}
