5d9bee12bba44af32e3e4c15cbe25d30608b847e
Lapin Pépère <83842166+Lapin-Sauvage@users.noreply.github.com>
2021-05-14 12:47:49 +0200

    System de Vie

---
+++ code/VieSystem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System_de_Vie
+{
+    class VieSystem
+    {
+
+        private float health = 100.000f;
+
+        
+        public VieSystem()
+        {
+
+            DeadSystem();
+
+        }
+
+        
+
+        public void DeadSystem()
+        {
+
+            if (health == 0 || health < 0)
+            {
+                // affichage HTML
+                // évent Respawn
+            }
+           
+        }
+
+        
+
+    }
+}
